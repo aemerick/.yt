e6e4f8acf9cd002f95c24f963e16ad63a8fca451
aemerick <aemerick11@gmail.com>
2016-07-27 18:10:11 -0400

    renaming and adding species fraction fields

--- my_plugins.py
+++ my_plugins.py
@@ -1,28 +1,23 @@
-#from yt.fields.api import ValidateParameter
-import yt
-from yt import physical_constants as const
-import numpy as np
 
-
-def _jeans_length(field,data):
+#def _jeans_length(field,data):
  #   if data.has_field_parameter("Gamma"):
   #      gamma = data.get_field_parameter('Gamma')
    # else:
     #    gamma = 1.6667
 
-    gamma = 1.6667
-    mu    = 1.3
-    T     = data['temperature'].convert_to_units('K')
-    c_s_squared = const.kboltz * gamma * T / (mu * const.mp)
+#    gamma = 1.6667
+#    mu    = 1.3
+#    T     = data['temperature'].convert_to_units('K')
+#    c_s_squared = const.kboltz * gamma * T / (mu * const.mp)
 
-    L = c_s_squared * np.pi / const.G / data['density']
+#    L = c_s_squared * np.pi / const.G / data['density']
 
-    L = np.sqrt(L)
+#    L = np.sqrt(L)
 
-    return L.convert_to_units('cm')
+#    return L.convert_to_units('cm')
 
-yt.add_field('jeans_length', function=_jeans_length, units='cm', force_override=True)
-add_field(('all','jl'), function=_jeans_length, units='cm', force_override=True)
+#yt.add_field('jeans_length', function=_jeans_length, units='cm', force_override=True)
+#add_field(('all','jl'), function=_jeans_length, units='cm', force_override=True)
 
 
 # ---------------------------------------------------------------------------
@@ -37,7 +32,15 @@ add_field(('all','jl'), function=_jeans_length, units='cm', force_override=True)
 #
 # ----------------------------------------------------------------------------
 def _metal_fraction(field, data):
-  metal_dens = data[('enzo', 'Metal_Density')].value
+
+  try:
+      metal_dens = data[('enzo', 'Metal_Density')].value
+  except:
+      try:
+          metal_dens = data[('enzo', 'metallicity')].value
+      except:
+          metal_dens = data[('enzo', 'Metallicity')].value
+
   metal_dens = metal_dens * data.ds.mass_unit / data.ds.length_unit**3
   metal_dens = metal_dens.convert_to_cgs()
 
@@ -46,8 +49,7 @@ def _metal_fraction(field, data):
   fraction = metal_dens / dens
 
   return fraction
-add_field('metal_gas_fraction', function = _metal_fraction, units='dimensionless')
-
+add_field(('gas','metal_fraction'), function = _metal_fraction, units='dimensionless')
 
 
 def _C_Fraction(field, data):
@@ -60,7 +62,7 @@ def _C_Fraction(field, data):
   fraction = metal_dens / dens
 
   return fraction
-add_field(('gas','C_Gas_Fraction'), function = _C_Fraction, units='dimensionless')
+add_field(('gas','C_Fraction'), function = _C_Fraction, units='dimensionless')
 
 
 def _O_Fraction(field, data):
@@ -73,7 +75,7 @@ def _O_Fraction(field, data):
   fraction = metal_dens / dens
 
   return fraction
-add_field(('gas','O_Gas_Fraction'), function = _O_Fraction, units='dimensionless')
+add_field(('gas','O_Fraction'), function = _O_Fraction, units='dimensionless')
 
 def _N_Fraction(field, data):
   metal_dens = data[('enzo', 'N_Density')].value
@@ -85,7 +87,7 @@ def _N_Fraction(field, data):
   fraction = metal_dens / dens
 
   return fraction
-add_field(('gas','N_Gas_Fraction'), function = _N_Fraction, units='dimensionless')
+add_field(('gas','N_Fraction'), function = _N_Fraction, units='dimensionless')
 
 def _Mg_Fraction(field, data):
   metal_dens = data[('enzo', 'Mg_Density')].value
@@ -97,7 +99,7 @@ def _Mg_Fraction(field, data):
   fraction = metal_dens / dens
 
   return fraction
-add_field('Mg_Gas_Fraction', function = _Mg_Fraction, units='dimensionless')
+add_field(('gas','Mg_Fraction'), function = _Mg_Fraction, units='dimensionless')
 
 def _Si_Fraction(field, data):
   metal_dens = data[('enzo', 'Si_Density')].value
@@ -109,7 +111,20 @@ def _Si_Fraction(field, data):
   fraction = metal_dens / dens
 
   return fraction
-add_field('Si_Gas_Fraction', function = _Si_Fraction, units='dimensionless')
+add_field(('gas','Si_Fraction'), function = _Si_Fraction, units='dimensionless')
+
+def _S_Fraction(field, data):
+  metal_dens = data[('enzo', 'S_Density')].value
+  metal_dens = metal_dens * data.ds.mass_unit / data.ds.length_unit**3
+  metal_dens = metal_dens.convert_to_cgs()
+
+  dens = data[('enzo','Density')].convert_to_units('g/cm**3')
+
+  fraction = metal_dens / dens
+
+  return fraction
+add_field(('gas','S_Fraction'), function = _Si_Fraction, units='dimensionless')
+
 
 def _Fe_Fraction(field, data):
   metal_dens = data[('enzo', 'Fe_Density')].value
@@ -121,7 +136,19 @@ def _Fe_Fraction(field, data):
   fraction = metal_dens / dens
 
   return fraction
-add_field(('gas','Fe_Gas_Fraction'), function = _Fe_Fraction, units='dimensionless')
+add_field(('gas','Fe_Fraction'), function = _Fe_Fraction, units='dimensionless')
+
+def _Ni_Fraction(field, data):
+  metal_dens = data[('enzo', 'Ni_Density')].value
+  metal_dens = metal_dens * data.ds.mass_unit / data.ds.length_unit**3
+  metal_dens = metal_dens.convert_to_cgs()
+
+  dens = data[('enzo','Density')].convert_to_units('g/cm**3')
+
+  fraction = metal_dens / dens
+
+  return fraction
+add_field(('gas','Ni_Fraction'), function = _Fe_Fraction, units='dimensionless')
 
 
 def _Ba_Fraction(field, data):
@@ -134,7 +161,7 @@ def _Ba_Fraction(field, data):
   fraction = metal_dens / dens
 
   return fraction
-add_field('Ba_Gas_Fraction', function = _Ba_Fraction, units='dimensionless')
+add_field(('gas','Ba_Fraction'), function = _Ba_Fraction, units='dimensionless')
 
 def _La_Fraction(field, data):
   metal_dens = data[('enzo', 'La_Density')].value
@@ -146,7 +173,7 @@ def _La_Fraction(field, data):
   fraction = metal_dens / dens
 
   return fraction
-add_field('La_Gas_Fraction', function = _La_Fraction, units='dimensionless')
+add_field(('gas','La_Fraction'), function = _La_Fraction, units='dimensionless')
 
 def _Y_Fraction(field, data):
   metal_dens = data[('enzo', 'Y_Density')].value
@@ -158,7 +185,7 @@ def _Y_Fraction(field, data):
   fraction = metal_dens / dens
 
   return fraction
-add_field('Y_Gas_Fraction', function = _Y_Fraction, units='dimensionless')
+add_field(('gas','Y_Fraction'), function = _Y_Fraction, units='dimensionless')
 
 def _Eu_Fraction(field, data):
   metal_dens = data[('enzo', 'Eu_Density')].value
@@ -170,113 +197,124 @@ def _Eu_Fraction(field, data):
   fraction = metal_dens / dens
 
   return fraction
-add_field('Eu_Gas_Fraction', function = _Eu_Fraction, units='dimensionless')
+add_field(('gas','Eu_Fraction'), function = _Eu_Fraction, units='dimensionless')
 
 
 # ---------------------------------------------------
   
 
->>>>>>> fccda75aac90b0bb265bc3f2d1fd8a2ba0b124b1
 def _CI_Density(field, data):
-  dens = data[('enzo', 'CI_Density')].value
+  dens = data[('enzo', 'C_Density')].value
   dens = dens * data.ds.mass_unit / data.ds.length_unit**3
   dens = dens.convert_to_cgs()
 
   return dens
 
-add_field('CI_Density_cgs', function=_CI_Density, units='g/cm**3',
+add_field('C_Density_cgs', function=_CI_Density, units='g/cm**3',
                         force_override = True)
 # #
 
 def _NI_Density(field, data):
-  dens = data[('enzo', 'NI_Density')].value
+  dens = data[('enzo', 'N_Density')].value
   dens = dens * data.ds.mass_unit / data.ds.length_unit**3
   dens = dens.convert_to_cgs()
 
   return dens
 
-add_field('NI_Density_cgs', function=_NI_Density, units='g/cm**3',
+add_field('N_Density_cgs', function=_NI_Density, units='g/cm**3',
                         force_override = True)
 
 # #
 
 def _OI_Density(field, data):
-  dens = data[('enzo', 'OI_Density')].value
+  dens = data[('enzo', 'O_Density')].value
   dens = dens * data.ds.mass_unit / data.ds.length_unit**3
   dens = dens.convert_to_cgs()
 
   return dens
 
-add_field('OI_Density_cgs', function=_OI_Density, units='g/cm**3',
+add_field('O_Density_cgs', function=_OI_Density, units='g/cm**3',
                         force_override = True)
 
 def _MgI_Density(field, data):
-  dens = data[('enzo', 'MgI_Density')].value
+  dens = data[('enzo', 'Mg_Density')].value
   dens = dens * data.ds.mass_unit / data.ds.length_unit**3
   dens = dens.convert_to_cgs()
 
   return dens
 
-add_field('MgI_Density_cgs', function=_MgI_Density, units='g/cm**3',
+add_field('Mg_Density_cgs', function=_MgI_Density, units='g/cm**3',
                         force_override = True)
 
 def _SiI_Density(field, data):
-  dens = data[('enzo', 'SiI_Density')].value
+  dens = data[('enzo', 'Si_Density')].value
   dens = dens * data.ds.mass_unit / data.ds.length_unit**3
   dens = dens.convert_to_cgs()
 
   return dens
 
-add_field('SiI_Density_cgs', function=_SiI_Density, units='g/cm**3',
+add_field('Si_Density_cgs', function=_SiI_Density, units='g/cm**3',
                         force_override = True)
 
 def _FeI_Density(field, data):
-  dens = data[('enzo', 'FeI_Density')].value
+  dens = data[('enzo', 'Fe_Density')].value
   dens = dens * data.ds.mass_unit / data.ds.length_unit**3
   dens = dens.convert_to_cgs()
 
   return dens
 
-add_field('FeI_Density_cgs', function=_FeI_Density, units='g/cm**3',
+add_field('Fe_Density_cgs', function=_FeI_Density, units='g/cm**3',
                         force_override = True)
 
-def _YI_Density(field, data):
-  dens = data[('enzo', 'YI_Density')].value
+def _NiI_Density(field, data):
+  dens = data[('enzo', 'Ni_Density')].value
   dens = dens * data.ds.mass_unit / data.ds.length_unit**3
   dens = dens.convert_to_cgs()
 
   return dens
 
-add_field('YI_Density_cgs', function=_YI_Density, units='g/cm**3',
+add_field('Ni_Density_cgs', function=_NiI_Density, units='g/cm**3',
+                        force_override = True)
+
+
+def _YI_Density(field, data):
+  dens = data[('enzo', 'Y_Density')].value
+  dens = dens * data.ds.mass_unit / data.ds.length_unit**3
+  dens = dens.convert_to_cgs()
+
+  return dens
+
+add_field('Y_Density_cgs', function=_YI_Density, units='g/cm**3',
                         force_override = True)
 
 def _LaI_Density(field, data):
-  dens = data[('enzo', 'LaI_Density')].value
+  dens = data[('enzo', 'La_Density')].value
   dens = dens * data.ds.mass_unit / data.ds.length_unit**3
   dens = dens.convert_to_cgs()
 
   return dens
 
-add_field('LaI_Density_cgs', function=_LaI_Density, units='g/cm**3',
+add_field('La_Density_cgs', function=_LaI_Density, units='g/cm**3',
                         force_override = True)
 
 def _BaI_Density(field, data):
-  dens = data[('enzo', 'BaI_Density')].value
+  dens = data[('enzo', 'Ba_Density')].value
   dens = dens * data.ds.mass_unit / data.ds.length_unit**3
   dens = dens.convert_to_cgs()
 
   return dens
 
-add_field('BaI_Density_cgs', function=_BaI_Density, units='g/cm**3',
+add_field('Ba_Density_cgs', function=_BaI_Density, units='g/cm**3',
                         force_override = True)
 
 def _EuI_Density(field, data):
-  dens = data[('enzo', 'EuI_Density')].value
+  dens = data[('enzo', 'Eu_Density')].value
   dens = dens * data.ds.mass_unit / data.ds.length_unit**3
   dens = dens.convert_to_cgs()
 
   return dens
 
-add_field('EuI_Density_cgs', function=_EuI_Density, units='g/cm**3',
+add_field('Eu_Density_cgs', function=_EuI_Density, units='g/cm**3',
                         force_override = True)
 # ---------------------------------------------------------------------------
+
